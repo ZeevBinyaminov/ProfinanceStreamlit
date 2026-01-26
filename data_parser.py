c7d49a459d38cc5b82db737e0ab3d1600d70da08
ZeevBinyaminov <zzbinyaminov@outlook.com>
2026-01-26 18:45:51 +0300

    no-sandbox uncommented

--- data_parser.py
+++ data_parser.py
@@ -20,8 +20,8 @@ from selenium.common.exceptions import (
 def get_driver_and_wait(timeout=8):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--blink-settings=imagesEnabled=false")
-    # chrome_options.add_argument("--headless=new")
-    # chrome_options.add_argument("no-sandbox")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("no-sandbox")
     chrome_options.add_argument("disable-dev-shm-usage")
 
     driver = webdriver.Chrome(options=chrome_options)
