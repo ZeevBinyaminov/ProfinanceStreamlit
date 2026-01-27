b1cd2c160cc565ed05c31364b66c0794ae21e776
ZeevBinyaminov <zzbinyaminov@outlook.com>
2026-01-27 13:16:46 +0300

    bugfix

--- data_parser.py
+++ data_parser.py
@@ -22,7 +22,7 @@ class Singleton:
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
 
